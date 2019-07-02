c64778fdb3fe0ef9b9e40a17d36a91a7f49e6c1f
Sky Wickenden <skywickenden@users.noreply.github.com>
2019-07-02 08:59:43 +0100

    .gitignore fix for mongo-init.js

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Routes from "./routes";
-
-
-const wrapper = document.getElementById("react-wrapper");
-ReactDom.render(<Routes />, wrapper);
--- client/src/pages/home.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "../../test-utils";
-import Home from "./home";
-
-it("Renders and has text content", () => {
-  const { getByText } = render(<Home />);
-  getByText("Home page");
-});
